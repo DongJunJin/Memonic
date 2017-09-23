04e42e8c25bff58eac060fe1753495a7ac4679f7
DongJunJin <D29Jin@edu.uwaterloo.ca>
2017-09-23 11:16:32 -0400

    Bad Build

--- app/src/main/java/memes/memonic/MainActivity.java
+++ app/src/main/java/memes/memonic/MainActivity.java
@@ -1,15 +1,26 @@
 package memes.memonic;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.microsoft.projectoxford.emotion.EmotionServiceClient;
+import com.microsoft.projectoxford.emotion.EmotionServiceRestClient;
+
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import okhttp3.MediaType;
@@ -22,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int CAMERA_REQUEST = 1888;
     private ImageView imageView;
+    private JSONObject jsonObject;
 
 
 
@@ -29,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         this.imageView = (ImageView) this.findViewById(R.id.imageView1);
         Button photoButton = (Button) this.findViewById(R.id.button1);
         photoButton.setOnClickListener(new View.OnClickListener() {
@@ -39,21 +52,43 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(cameraIntent, CAMERA_REQUEST);
             }
         });
+
+//        EmotionServiceRestClient
+
+        ConnectivityManager cm =
+                (ConnectivityManager)getBaseContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+
+        Log.d("Network", isConnected ? "works" : "doesnt work" );
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(photo);
-            Requester requester = new Requester();
-            String json = requester.returnJson();
-            String response = null;
-            try {
-                response = requester.post("https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize", json);
-            } catch (IOException e) {
-                e.printStackTrace();
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            photo.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] imageBytes = baos.toByteArray();
+//            String imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+            if (requestCode == 1 && data != null) {
+                if (resultCode == RESULT_OK) {
+                    Requester requester = new Requester();
+
+                    String response = null;
+                    try {
+                        response = requester.post("https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize", imageBytes);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Log.d("Memes", response);
+                }
+                if (requestCode == RESULT_CANCELED) {
+                }
             }
-            Log.d("Memes", response);
         }
     }
 }
--- app/src/main/java/memes/memonic/Requester.java
+++ app/src/main/java/memes/memonic/Requester.java
@@ -1,6 +1,9 @@
 package memes.memonic;
 
 import android.content.res.Resources;
+import android.support.annotation.Nullable;
+
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -16,23 +19,28 @@ import okhttp3.Response;
 
 public class Requester {
 
-    public static final MediaType JSON
-            = MediaType.parse("application/json; charset=utf-8");
+    public static final MediaType contentType
+            = MediaType.parse("application/octet-stream");
 
     OkHttpClient client = new OkHttpClient();
 
-    String post(String url, String json) throws IOException {
-        RequestBody body = RequestBody.create(JSON, json);
+    String post(String url, byte[] json) throws IOException {
+        RequestBody body = RequestBody.create(contentType, json);
         Request request = new Request.Builder()
                 .url(url)
-                .addHeader("Content-Type", "application/json")
-                .addHeader("Ocp-Apim-Subscription-Key", Resources.getSystem().getString(R.string.Sub_key))
+                //.addHeader("Content-Type", "application/octet-stream")
+                .addHeader("Ocp-Apim-Subscription-Key", Resources.getSystem().getString(R.string.Sub_key)) //Put subcription key here
                 .post(body)
                 .build();
         try (Response response = client.newCall(request).execute()){
             return response.body().string();
+
+        }catch (IOException e){
+            e.printStackTrace();
+            return "1";
         }
     }
+
 
     String returnJson(){
         return (Resources.getSystem().getString(R.string.Example));
